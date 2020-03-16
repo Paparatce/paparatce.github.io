08d0b245522277e9955cf0066cfaf995244ccf74
Paparatce <59749622+Paparatce@users.noreply.github.com>
2020-03-16 21:14:46 +0200

    Add files via upload

--- start2/js/main.js
+++ start2/js/main.js
@@ -1,11 +1,32 @@
-$(function() {
+$(function () {
 
-    $('.list__inner .tab, .reviews__tab').on('click', function(event) {
-        var id = $(this).attr('data-id');
-        $('.list__inner, .reviews__inner').find('.tab-item, .reviews__content-item ').removeClass('active-tab').hide();
-        $('.list__inner .tabs, .reviews__tabs').find('.tab , .reviews__tab').removeClass('active');
-        $(this).addClass('active');
-        $('#' + id).addClass('active-tab').fadeIn();
-        return false;
+    $(".top__slider").slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplayspeed: 3000,
+        fade: true,
+        responsive: [
+            {
+                breakpoint: 601,
+                settings: {
+                    dots: false
+                }
+            }
+        ]
     });
+    $('.menu__btn').click(function () {
+        $('.header__menu-list').slideToggle();
+    });
+
+
+$(".top__to-go").on("click","a", function(event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+    top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 900);
+
 });
+
+
+ });
